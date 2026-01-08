f073cd939b5a8b44bc2a328a9aacc9c35b5f26de
RajLaxmiSingh22 <rajlaxmi@drify.com>
2026-01-09 05:21:27 +0530

    chore: add express dependency to package.json

--- index.js
+++ index.js
@@ -1,50 +1,34 @@
-import http from "http";
+import express from "express";
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const app = express();
 
-  // Home page
-  if (url === "/" && method === "GET") {
-    res.setHeader("Content-Type", "text/html");
-    return res.end(`
-      <form action="/create-user" method="POST">
-        <label>Name:</label>
-        <input type="text" name="username" />
-        <button type="submit">Submit</button>
-      </form>
-    `);
-  }
+// Parse form data: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
 
-  // Handle form submit
-  if (url === "/create-user" && method === "POST") {
-    const dataChunk = [];
-
-    req.on("data", (chunk) => {
-        log("Chunk received:", chunk);
-      dataChunk.push(chunk);
-    });
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(dataChunk).toString(); // username=John
-      const username = parsedBody.split("=")[1];
-
-      console.log("Username:", decodeURIComponent(username));
-
-      res.statusCode = 302;
-      res.setHeader("Location", "/");
-      return res.end();
-    });
-
-    return;
-  }
-
-  // Not found
-  res.statusCode = 404;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Page Not Found</h1>");
+// Home page
+app.get("/", (req, res) => {
+  res.send(`
+    <form action="/create-user" method="POST">
+      <label>Name:</label>
+      <input type="text" name="username" />
+      <button type="submit">Submit</button>
+    </form>
+  `);
 });
 
-server.listen(3000, () => {
+// Handle form submit
+app.post("/create-user", (req, res) => {
+  const { username } = req.body;
+  console.log("Username:", username);
+
+  return res.redirect("/");
+});
+
+// Not found (404)
+app.use((req, res) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
+
+app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
